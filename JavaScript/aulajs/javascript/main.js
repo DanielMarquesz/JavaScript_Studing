b9b5b3ddea2cbd3f4a05384cbdbf0f45f7c1dd8e
Daniel Marques <daniel.marques142@gmail.com>
2020-11-01 22:48:59 -0300

    Objects Interations

--- JavaScript/aulajs/javascript/main.js
+++ JavaScript/aulajs/javascript/main.js
@@ -101,5 +101,100 @@
 
 /*---- FUNÇÕES DATE ----*/
 
-var date = new Date() // Instânciação da Classe Date, permite a chamada de várias formatações de datas/anos/dias/horas
-alert(date.getHours())
+// var date = new Date() // Instânciação da Classe Date, permite a chamada de várias formatações de datas/anos/dias/horas
+// alert(date.getHours())
+
+
+/*------------------------------- AULA 03 -------------------------------*/
+
+
+
+/*---- FUNÇÕES ----*/
+
+// function soma(n1, n2){  // Soma dois Inteiros
+//     var res = n1 + n2
+//     return res
+// }
+
+// function firstUpper(phrase){ // Primeira Letra de uma frase maiúscula
+
+//     var phrase = phrase.substring(0,1).toUpperCase().concat(phrase.substring(1));
+   
+//     alert(phrase)
+// }
+
+
+// function setReplace(phrase, word, new_word){
+
+//     phrase = phrase.toUpperCase()
+//     word = word.toUpperCase()
+//     var res = phrase.replace(word, new_word)
+//     res = res.toLowerCase()
+    
+//     return res
+// }
+// firstUpper("palavra cantada")
+// alert(soma(12, 18))
+
+// alert(setReplace("Pega o Jooj", "pega", "Captura"))
+
+/*------------------------------- AULA 04 -------------------------------*/
+
+var that = this
+alert(that.Date())
+
+function validateNumber(){  // Função que não permite números e nem campos vazios
+   
+    var msg = document.getElementById("texto").value
+
+
+    for (letra of msg){
+
+        if (!isNaN(letra)){
+
+            alert("Números não são permitidos!")
+            document.getElementById("texto").value=""
+            return;
+        }
+
+    }if (msg === ""){
+
+        alert("Você não digitou nada!")
+        document.getElementById("texto").value=""
+
+    }else if (!isNaN(msg)){
+        
+        alert("É um número!")
+        document.getElementById("texto").value=""
+
+    }else if (msg != ""){
+
+        alert("Você Digitou: " + msg)   
+        document.getElementById("texto").value=""
+
+    }
+
+    document.getElementById("muda").innerHTML = "Obrigado!"
+    
+    
+}
+
+function redirect(){        // Redireciona para um site
+   // window.open("https://www.google.com.br/") // Abre uma nova guia
+    window.location.href = "https://www.google.com.br/" // Abre o na mesma guia
+}
+
+function muda(oEvent){ // oEvent = Faz referência ao próprio objeto o qual pertence
+    //document.getElementById("move").innerHTML = "Obrigado por passar por aqui!"
+    oEvent.innerHTML = "Obrigado por passar aqui!"
+}
+
+function volta(oEvent){ // oEvent = Faz referência ao próprio objeto o qual pertence
+    //document.getElementById("move").innerHTML = "Passe o mouve novamente!"
+    oEvent.innerHTML = "Passe o mouse novamente!"
+}
+
+function mudaNome(oEvent){  // Mostra ou passa um valor quando ocorre alguma alteração
+    console.log(oEvent.value)
+
+}
